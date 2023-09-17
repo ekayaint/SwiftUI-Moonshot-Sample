e178fa7ab538fab87fc4693d02a84739c26e1ef5
ekayaint <ekayaint@gmail.com>
2023-09-17 20:10:52 +0300

    Bundle extension is ok

---
+++ SwiftUI-Moonshot-Sample/Model/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  SwiftUI-Moonshot-Sample
+//
+//  Created by ekayaint on 17.09.2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
