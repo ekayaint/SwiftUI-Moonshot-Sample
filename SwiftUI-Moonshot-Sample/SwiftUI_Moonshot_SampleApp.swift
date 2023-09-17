384329aba6c58babdc1b089eee184ec3e11257f6
ekayaint <ekayaint@gmail.com>
2023-09-17 19:35:25 +0300

    Initial Commit

---
+++ SwiftUI-Moonshot-Sample/SwiftUI_Moonshot_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Moonshot_SampleApp.swift
+//  SwiftUI-Moonshot-Sample
+//
+//  Created by ekayaint on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Moonshot_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
